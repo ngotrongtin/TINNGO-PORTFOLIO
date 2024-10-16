fc24791eab642c517e648665fb9b20e6a1c54d6d
ngotrongtin <tigopro.1703@gmail.com>
2024-10-16 17:25:46 +0700

    update cv

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ app.use(express.static('public'));
 // Define a route for the homepage
 app.get('/', (req, res) => {
   // Sử dụng path.join để kết hợp đường dẫn của thư mục hiện tại và tên file HTML
-  res.sendFile(path.join(__dirname, 'test.html'));
+  res.sendFile(path.join(__dirname, 'test2.html'));
 });
 
 const PORT = process.env.PORT || 3000;
